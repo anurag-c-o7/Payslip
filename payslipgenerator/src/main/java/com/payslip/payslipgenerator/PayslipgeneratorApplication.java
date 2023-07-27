622b04afb8cd996a9b08ed1226c408ed5154194a
anurag <anurag.chaudhari@noovosoft.com>
2023-07-27 23:12:38 +0530

    added context

--- payslipgenerator/src/main/java/com/payslip/payslipgenerator/PayslipgeneratorApplication.java
+++ payslipgenerator/src/main/java/com/payslip/payslipgenerator/PayslipgeneratorApplication.java
@@ -3,6 +3,7 @@ package com.payslip.payslipgenerator;
 import com.payslip.payslipgenerator.beans.UserProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PayslipgeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PayslipgeneratorApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(PayslipgeneratorApplication.class, args);
 //		- id
 //				- first name
 //				- last name
